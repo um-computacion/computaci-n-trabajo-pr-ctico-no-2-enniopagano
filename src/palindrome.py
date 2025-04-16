8f91852e36a9e40b17e1475afe229a899b98eff5
enniopagano <e.pagano@alumno.um.edu.ar>
2025-04-16 08:39:01 -0300

    Implementacion de metodos para limpieza de texto

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,4 +1,6 @@
 def is_palindrome(word: str):
+    word = word.lower() # Para ignorar mayusculas y minisculas
+    word = word.strip() # Para ignorar espacios en blanco
     if word[0] == word[-1]:
         return True
     return False
